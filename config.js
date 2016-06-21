1f49ac2d45198cb7a3722ad54bd0236b1b2589e5
Reid <jreid.greer@gmail.com>
2016-06-21 11:26:59 -0700

    Implement working hello world

---
+++ config.js
@@ -0,0 +1,7 @@
+const port = 3000;
+const name = 'Illinois Data';
+
+module.exports = {
+  port,
+  name
+}
--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-server.js
+const express = require('express');
+const app = express();
+
+// Load config files
+const config =  require('./config');
+
+app.use(express.static('client'));
+
+app.listen(config.port, function () {
+  console.log(`${config.name} listening on port ${config.port}!`);
+});
